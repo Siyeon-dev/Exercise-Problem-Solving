9988e787518995f9f78f28f1d3977ec0b1b402eb
Siyeon-dev <siyeon.dev@gmail.com>
2021-06-14 20:11:07 +0900

    1157 : 단어 공부(re)

--- 1157.js
+++ 1157.js
@@ -1,32 +1,30 @@
-// 소요시간 25분
+/**
+ * 문자 값을 index 값으로 1:1 대응시켜 문제를 풀 수 있다.
+ */
+
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-const inputString = input[0];
-let lowerCase = "";
-let cntAlphaObj = {};
+const input = fs.readFileSync("/dev/stdin").toString().toLowerCase();
+const alphabetList = Array(26).fill(0);
 
-for (let char of inputString) {
-	lowerCase += char.toLocaleLowerCase();
+for (let char of input) {
+	// charCodeAt() -> 해당 값에 대한 UTF-16 값 반환
+	alphabetList[char.charCodeAt() - 97]++;
 }
 
-for (let char of lowerCase) {
-	cntAlphaObj[char] = 0;
-}
+// Math.max -> 여러 값 중 가장 큰 값을 반환
+// 스프레드 연산자로 배열 속 값들 중 가장 큰 값을 뽑을 수 있다.
+const maxValue = Math.max(...alphabetList);
+// indexOf -> 인자 값을 value로 가지는 index 반환
+const maxNumInx = alphabetList.indexOf(maxValue);
 
-for (let key in cntAlphaObj) {
-	for (let char of lowerCase) {
-		if (key === char) cntAlphaObj[char] = cntAlphaObj[char] + 1;
+let isSame = false;
+
+for (let i = 0; i < alphabetList.length; i++) {
+	if (maxValue === alphabetList[i] && i !== maxNumInx) {
+		isSame = !isSame;
+		break;
 	}
 }
 
-let sortedAlphaList = [];
-
-for (let value in cntAlphaObj)
-	sortedAlphaList.push([value, cntAlphaObj[value]]);
-
-sortedAlphaList.sort((a, b) => b[1] - a[1]);
-
-if (sortedAlphaList[1] !== undefined)
-	if (sortedAlphaList[0][1] === sortedAlphaList[1][1]) console.log("?");
-	else console.log(sortedAlphaList[0][0].toUpperCase());
-else console.log(sortedAlphaList[0][0].toUpperCase());
+// fromCharCode 를 통해서 UTF-16값을 다시 문자로 parsing
+console.log(isSame ? "?" : String.fromCharCode(maxNumInx + 65));
